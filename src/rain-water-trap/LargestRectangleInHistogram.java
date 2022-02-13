44ecf1174498ce80ca265cd27a8e195152f9c38e
Jayaraj <jayarajsgoudar@gmail.com>
2022-02-13 11:56:55 +0530

    LRUCache - Feb 13 2022

--- src/rain-water-trap/LargestRectangleInHistogram.java
+++ src/rain-water-trap/LargestRectangleInHistogram.java
@@ -1,5 +1,28 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
+        Stack<Integer[]> stack = new Stack<>();// (value, index)
+        int maxArea = 0;
+
+        for (int i = 0; i < heights.length; i++) {
+            int tempI = i;
+            while (!stack.isEmpty() && stack.peek()[0] > heights[i]) {
+                Integer[] lastMax = stack.pop();
+                maxArea = Math.max(maxArea, lastMax[0] * (i - lastMax[1]));
+                tempI = lastMax[1];
+            }
+            stack.push(new Integer[]{heights[i], tempI});
+        }
+
+        for (Integer[] monoIncr : stack) {
+            maxArea = Math.max(maxArea, monoIncr[0] * (heights.length - monoIncr[1]));
+        }
+
+        return maxArea;
+    }
+}
+
+class Solution {
+    public int largestRectangleArea(int[] heights) {
         int max = 0;
         int n = heights.length;
         Deque<Integer> stack = new ArrayDeque<>();
