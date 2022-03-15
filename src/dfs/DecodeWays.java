5d0e48b340035ea92dbe33f91977a4042e050296
Jayaraj <jayarajsgoudar@gmail.com>
2022-03-15 19:06:51 +0530

    DecodeWays - Mar 15th 2022

---
+++ src/dfs/DecodeWays.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int numDecodings(String s) {
+        Integer[] memo = new Integer[s.length() + 1];
+        return dfs(0, s, memo);
+    }
+
+    private int dfs(int start, String s, Integer[] memo) {
+        if (memo[start] != null) {
+            return memo[start];
+        }
+        int n = s.length();
+
+        if (start == n) return 1;
+        if (s.charAt(start) == '0') return 0;
+
+        int ways = dfs(start + 1, s, memo);
+        if (start < n - 1 && (s.charAt(start) == '1' || s.charAt(start) == '2' && s.charAt(start + 1) < '7')) {
+            ways += dfs(start + 2, s, memo);
+        }
+
+        memo[start] = ways;
+        return ways;
+    }
+}
+
+class Solution {
+    public int numDecodings(String s) {
+        int n = s.length();
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = s.charAt(0) == '0' ? 0 : 1;
+
+        for (int i = 2; i <= n; i++) {
+            String lastOne = s.substring(i - 1, i);
+            String lastTwo = s.substring(i - 2, i);
+
+            if (Integer.parseInt(lastOne) >= 1) {
+                dp[i] += dp[i - 1];
+            }
+
+            if (Integer.parseInt(lastTwo) >= 10 && Integer.parseInt(lastTwo) <= 26) {
+                dp[i] += dp[i - 2];
+            }
+        }
+
+        return dp[n];
+    }
+}
