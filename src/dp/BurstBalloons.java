3cdb8465bb80d50a807fc4058110da51efb06640
Jayaraj <jayarajsgoudar@gmail.com>
2022-03-24 09:22:50 +0530

    24 Mar 2022 - BurstBalloons

--- src/dp/BurstBalloons.java
+++ src/dp/BurstBalloons.java
@@ -44,3 +44,77 @@ class Solution {
         return nums[i];
     }
 }
+
+class Solution {
+    public int maxCoins(int[] nums) {
+        int[] copy = new int[nums.length + 2];
+
+        int j = 1;
+        for (int num : nums) {
+            if (num > 0) {
+                copy[j++] = num;
+            }
+        }
+        copy[0] = 1;
+        copy[j] = 1;
+
+        return recursion(copy, 0, j);
+    }
+
+    private int recursion(int[] nums, int start, int end) {
+        if (end - start < 1) {
+            return 0;
+        }
+
+        int maxCoins = 0;
+        for (int i = start + 1; i < end; i++) {
+            maxCoins = Math.max(
+                    maxCoins,
+                    nums[start] * nums[i] * nums[end] + recursion(nums, start, i) + recursion(nums, i, end)
+            );
+        }
+
+        return maxCoins;
+    }
+}
+
+// Add memo
+class Solution {
+    int[][] memo;
+
+    public int maxCoins(int[] nums) {
+        int[] copy = new int[nums.length + 2];
+
+        int j = 1;
+        for (int num : nums) {
+            if (num > 0) {
+                copy[j++] = num;
+            }
+        }
+        copy[0] = 1;
+        copy[j] = 1;
+
+        memo = new int[j + 1][j + 1];
+        return recursion(copy, 0, j);
+    }
+
+    private int recursion(int[] nums, int start, int end) {
+        if (end - start < 1) {
+            return 0;
+        }
+        if (memo[start][end] != 0) {
+            return memo[start][end];
+        }
+
+        int maxCoins = 0;
+        for (int i = start + 1; i < end; i++) {
+            maxCoins = Math.max(
+                    maxCoins,
+                    nums[start] * nums[i] * nums[end] + recursion(nums, start, i) + recursion(nums, i, end)
+            );
+        }
+        memo[start][end] = maxCoins;
+
+        return maxCoins;
+    }
+}
